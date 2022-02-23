b5623c95cbe66a9fae597098d4fa2a93b093f87d
José Eyzaguirre <josemanuel.eyzaguirre@gmail.com>
2022-02-22 22:53:56 -0300

    avance

--- server.js
+++ server.js
@@ -1,17 +1,47 @@
 const express = require('express');
 const uuid = require('uuid');
 const fs = require('fs').promises;
+const axios = require('axios');
 
 const app = express();
 
 app.use(express.static('static'));
+app.use( express.json() );
+app.use( express.urlencoded({ extended: true }) );
 
-app.get('/roommate', async (req, res) => {
+app.post('/roommate', async (req, res) => {
+
+    const datos = await axios.get('https://randomuser.me/api/');
+    const userData = datos.data.results[0];
+
+    let id = uuid.v4();
     
+    const usuarioRandom = {
+        nombre: userData.name.first + " " + userData.name.last,
+        id: id,
+        debe: 0,
+        recibe: 0        
+    }
+
+    let db = await fs.readFile('db.json', 'utf-8');
+    db = JSON.parse(db);
+    db.roommates.push(usuarioRandom)
+
+    await fs.writeFile('db.json', JSON.stringify(db), 'utf-8')
+
+    res.json(db)
 })
 
-app.get('/gastos', async (req, res) => {
+app.get('/roommates', (req, res) => {   
+    let db = require('./db.json')
+    let roommates = db.roommates
+    res.json({roommates})
+})
 
+app.get('/gastos', (req, res) => {
+    let db = require('./db.json')
+    let gastos = db.gastos
+    res.json({gastos})
 })
 
 app.listen(3000, () => {
